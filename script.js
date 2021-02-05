e700779cfcb8b8b78b4190512dc86f8411325b75
Autaev <95.website@mail.ru>
2021-02-05 15:32:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+//текст с помщью alert
+let a = 20;
+alert (a);  
+
+// вывод в консоль 
+let b = 30;
+console.log (b);
+ 
+b = 40; 
+console.log (b);
+
+
+let z = 40; // z = 40
+let x = 20; // x = 20
+x = z; // x = 40, z = 40
+z = 50; // z = 50
+console.log(x); // x = 40
+console.log(z); // z = 50
+
+// константа 
+const h = 70;
+console.log(h);
+
+
+
+// правильная переменная 
+let al = 145;
+let af = 120;
+let at = 215 ;
+
+at = af; // at = 120, af = 120
+af = 30; // af = 30
+al = at; //al = 120, at = 120
+al = af; // al = 30, af = 30
+af = 160; // af = 160
+at = 60; // at = 60
+al = af; // al = 160, af = 160
+at = al; // at = 160, al = 160
+console.log (at);
+console.log (al);
+alert (af);
+/*
+И ху 
+дара)) 
+*/
+
+// как по другому? просто ошибку допустить, и все?
+let hr = 45
+let ht = 30
+hr = 55 
+let ht = 59
+console.log (ht)
+
+
+// неправильная
+const y = 45 
+const o = 50
+o = y
+
